9559fb7e2edf13a5516c6c6754ee1bd8c99e80a3
ahotb <ah.r9625@gmail.com>
2026-09-22 16:24:10 +0300

    update

--- c++/Hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Programming is fun!" << endl;
-    cout << "Fundamentals First" << endl;
-    cout << "Problem Driven" << endl; // Display Problem Driven
-    return 0;
-}
--- c++/number.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "(10.5 + 2 * 3) / (45 - 3.5) = "; 
-    cout << (10.5 + 2 * 3) / (45 - 3.5) << endl; // Display the result of expression
-
-   return 0;
-}
