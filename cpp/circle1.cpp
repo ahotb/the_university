b50a68ea13e5ea9ce09c3d324e561bf25c813b7e
ahotb <ah.r9625@gmail.com>
2026-09-22 16:23:32 +0300

    pudate the lecture

---
+++ cpp/circle1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    // double radius;
+    // double area;
+    // const double PI = 3.14159;
+    // cout << "Enter a number : ";
+    // cin >> radius;
+
+    // area = radius * radius * PI;
+
+    // cout << "The area is ";
+    // cout << area << endl;
+   
+    cout << pow(8,1) << endl;
+    
+    return 0;
+}
---
+++ cpp/lecture-2/rectangular.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+  double length = 10;
+  double width = 15;
+
+  double result = length * width;
+
+  cout << "The rectangular is " << result << endl;
+
+  return 0;
+}
