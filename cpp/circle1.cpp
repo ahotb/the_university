3766e19df6a824e29646352409fd6556fc434aeb
ahotb <ah.r9625@gmail.com>
2026-09-24 22:54:17 +0300

    test code cpp

--- cpp/circle1.cpp
+++ cpp/circle1.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 
 int main(){
-    // double radius;
-    // double area;
-    // const double PI = 3.14159;
-    // cout << "Enter a number : ";
-    // cin >> radius;
-
-    // area = radius * radius * PI;
-
-    // cout << "The area is ";
-    // cout << area << endl;
-   
-    cout << pow(8,1) << endl;
-    
-    return 0;
-}
+    double radius;
+    double area;
+    const double PI = 3.14159;
+    cout << "Enter a number : ";
+    cin >> radius;
+    if(radius >= 0){
+    area = radius * radius * PI;
+    cout << "The area is ";
+    cout << area << endl;
+   }else{
+    cout << "This is not true" << endl;
+   }
+   }
